63e3e6f6dee4c74a71d2eb3d721334bea0029510
Billy Derringer <billyderringer7@gmail.com>
2018-03-13 16:37:32 -0500

    added header and state selector components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CompassIcon from './components/icon';
+import StateSelector from './components/stateSelector'
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location : 'Select State',
+        };
+    }
+
+    changeLocation(location){
+        this.setState({location: location});
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <div className="text-center" style={{height:'30vh'}}>
+            <CompassIcon width="60px" height="60px"/>
+            <h1 style={{fontVariant:'small-caps',color:'#fff'}}>Find Your Adventure</h1>
+            <h2 style={{color:'#fff',fontSize:'1.25em'}}>U.S. National Parks Finder</h2>
+        </div>
+        <div style={{height:'70vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <StateSelector handleChangeLocation={this.changeLocation.bind(this)} location={this.state.location}/>
+        </div>
+
+
       </div>
     );
   }
